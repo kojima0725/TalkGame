d38127fd74b552d23ae71fae6a942ae19ab85966
kojima0725 <kojikoji0725kt@gmail.com>
2022-02-15 04:32:05 +0900

    選択肢アクション機能を作成中

---
+++ Assets/Scripts/Talk/TalkCtrl/Selector/ButtonSelectAction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSelectAction : ITalkOrder
+{
+    public bool Doing => throw new System.NotImplementedException();
+
+    public void Do(TalkManager talkManager)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RequestSkip()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateDo()
+    {
+        throw new System.NotImplementedException();
+    }
+}
